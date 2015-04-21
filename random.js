89f09bc557006c4c9ff2b3a0bdb3fd5341c80646
Jvidalal <juan.vidal.alegria@alumnos.upm.es>
2015-04-21 18:49:40 +0200

    Version 3

--- random.js
+++ random.js
@@ -1,7 +1,6 @@
 var numero=Math.random();
+var cadena='mayor que 0.5'
 if (numero <=0.5 ){
-	console.log('menor que 0.5');
+	cadena='menor que 0.5';
 }
-else{
-	console.log('mayor que 0.5');
-}
+console.log(cadena);
